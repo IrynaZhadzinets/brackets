cb5bc7c4bd01125df641d9f92b1e47bb0287a086
IrynaZhadzinets <iryna.zhadzinets@gmail.com>
2019-03-04 20:39:48 +0300

    add function which check brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let i, stringLength, regex, pairsBrackets, num;
+  if (str.length % 2 != 0){
+    return false;
+  }
+  do {
+    stringLength = str.length;
+    for (i = 0; i < bracketsConfig.length; i++) {
+      num = +bracketsConfig[i].join('');
+      if (num == bracketsConfig[i].join(''))
+        pairsBrackets = bracketsConfig[i].join('');
+      else
+        pairsBrackets = "\\" + bracketsConfig[i][0] + "\\" + bracketsConfig[i][1];
+      regex = new RegExp(pairsBrackets, 'g');
+      str = str.replace(regex, '');
+    }
+    } while (stringLength != str.length);
+    if(str.length == 0) {
+      return true;
+    }
+    else
+      return false;
+  }
